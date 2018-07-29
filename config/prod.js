72107551c6c90dc4295d553ef751bb89c18cf1e3
Fangming Lu <cjlfm007@gmail.com>
2018-07-29 02:57:10 -0700

    tweak google strategy setting

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 //production keys here
 module.exports = {
-  googleClientID:process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI:process.env.MONGO_URI,
-  cookieKey:process.env.COOKIE_KEY,
-  googleRedirectURI: 'https://bayou.herokuapp.com'
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  googleRedirectURI: "https://immense-sea-23429.herokuapp.com"
 };
